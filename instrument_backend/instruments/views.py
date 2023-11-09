4c15454f641d6ca63847eb831d35c446e929989f
lanbow93 <the.doctor1501@gmail.com>
2023-11-09 15:30:02 -0600

    basic view created and 'deployed' pre cors

--- instrument_backend/instruments/views.py
+++ instrument_backend/instruments/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Instrument
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import InstrumentSerializer
+
+
+class InstrumentViewSet(viewsets.ModelViewSet):
+    ## The Main Query for the index route
+    queryset = Instrument.objects.all()
+    # The serializer class for serializing output
+    serializer_class = InstrumentSerializer
+    # optional permission class set permission level
+    permission_classes = [permissions.AllowAny] #Coule be [permissions.IsAuthenticated]
