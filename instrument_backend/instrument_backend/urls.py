e01c78be1ba85b9d3ef69b47729205161c30b32a
lanbow93 <the.doctor1501@gmail.com>
2023-11-10 07:19:23 -0600

    edited model removing seller information

--- instrument_backend/instrument_backend/urls.py
+++ instrument_backend/instrument_backend/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from instruments.views import InstrumentViewSet
+from django.conf import settings
+from django.conf.urls.static import static
 
 # create a new router
 router = routers.DefaultRouter()
@@ -14,3 +16,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
